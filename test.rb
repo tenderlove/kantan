4194f68c873996f347044ff3468fe4c1d56fe348
Aaron Patterson <tenderlove@ruby-lang.org>
2026-02-11 16:00:30 -0800

    add server

---
+++ test.rb
@@ -0,0 +1,24 @@
+require "htwo"
+require "socket"
+require "logger"
+
+class MyApp < HTWO::Handler
+  def on_request stream
+    stream.respond [[":status", "200"]], body: "hello"
+  end
+end
+
+server = TCPServer.new "127.0.0.1", 8888
+port = server.addr[1]
+
+logger = Logger.new $stderr
+logger.debug "port #{port}"
+
+while true
+  client = server.accept
+  client.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+
+  logger.debug "new connection"
+  session = HTWO::Session.new(client, handler: MyApp.new)
+  session.receive
+end
