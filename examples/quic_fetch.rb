1503f15b39314bee6fdf2e90cdffaf3e47593e2c
Aaron Patterson <tenderlove@ruby-lang.org>
2026-02-20 15:11:24 -0800

    add a fetch example

---
+++ examples/quic_fetch.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+# Example: fetch a page over HTTP/3 (QUIC).
+#
+# Usage:
+#   fish -c 'chruby ruby-master; ruby -Ilib examples/quic_fetch.rb [host] [port]'
+#
+# Defaults to localhost:4433 (matching test/quic_server.rb).
+
+require "kantan"
+require "kantan/h3"
+require "kantan/quic/client_connection"
+
+class ResponseHandler < Kantan::Handler
+  def initialize
+    @responses = {}
+    @done = Thread::Queue.new
+  end
+
+  def on_headers(stream)
+    @responses[stream.id] ||= { headers: stream.headers, body: +"" }
+    @responses[stream.id][:headers] = stream.headers
+  end
+
+  def on_data(stream, chunk)
+    @responses[stream.id] ||= { headers: [], body: +"" }
+    @responses[stream.id][:body] << chunk
+  end
+
+  def on_request(stream)
+    @done << stream.id
+  end
+
+  def on_close
+    @done << nil
+  end
+
+  def wait_for_stream(stream_id)
+    loop do
+      id = @done.pop
+      return @responses[stream_id] if id == stream_id || id.nil?
+    end
+  end
+end
+
+host = ARGV[0] || "localhost"
+port = (ARGV[1] || 4433).to_i
+
+conn = Kantan::QUIC::ClientConnection.new(host, port)
+conn.connect
+
+handler = ResponseHandler.new
+session = Kantan::H3::Session.new(conn, handler: handler)
+Thread.new { session.connect }
+
+# Give H3 control/QPACK streams time to initialize
+sleep 0.1
+
+stream_id = session.request([
+  [":method",    "GET"],
+  [":path",      "/"],
+  [":scheme",    "https"],
+  [":authority", host],
+  ["user-agent", "kantan-quic/1.0"],
+  ["accept",     "*/*"],
+])
+
+response = handler.wait_for_stream(stream_id)
+
+status = response[:headers].find { |n, _| n == ":status" }&.last
+puts "Status: #{status}"
+puts
+response[:headers].each { |name, value| puts "#{name}: #{value}" }
+puts
+puts response[:body]
+
+conn.close
